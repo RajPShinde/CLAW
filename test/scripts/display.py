150b460e55d9db66344f4d30562b435fbc18a26f
RajPShinde <rajprakashshinde07@gmail.com>
2023-07-03 05:59:18 -0700

    Restructure script directory

---
+++ test/scripts/display.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3.8
+from luma.core.interface.serial import i2c, spi, pcf8574
+from luma.core.interface.parallel import bitbang_6800
+from luma.core.render import canvas
+from luma.oled.device import sh1106
+from time import sleep
+import sys
+from pathlib import Path
+import av
+import PIL
+
+serial = i2c(port=1, address=0x3C)
+
+device = sh1106(serial)
+print(device.width)
+
+print(device.height)
+
+video_path = str(Path(__file__).resolve().parent.joinpath('../assets/logo_screen_white.mp4'))
+
+clip = av.open(video_path)
+
+for frame in clip.decode(video=0):
+
+    img = frame.to_image()
+    if img.width != device.width or img.height != device.height:
+
+        size = device.width, device.height
+        img = img.resize(size, PIL.Image.ANTIALIAS)
+
+    device.display(img.convert(device.mode))
